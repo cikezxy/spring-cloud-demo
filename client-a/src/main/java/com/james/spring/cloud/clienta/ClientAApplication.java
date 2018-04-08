9b6da2610dc3f6c9fbd7d5054b56cd067abb0d3d
zhengxiaoyue <zhengxiaoyue@dpandora.cn>
2018-04-08 08:42:58 +0800

    add eureka server/client

--- client-a/src/main/java/com/james/spring/cloud/clienta/ClientAApplication.java
+++ client-a/src/main/java/com/james/spring/cloud/clienta/ClientAApplication.java
@@ -2,7 +2,9 @@ package com.james.spring.cloud.clienta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ClientAApplication {
 
