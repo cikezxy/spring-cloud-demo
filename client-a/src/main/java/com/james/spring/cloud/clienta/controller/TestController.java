375aa7017991f86f5bd1e309948dbfbe7b7ebff6
cikezxy <cikezxy@163.com>
2019-03-30 18:22:05 +0800

    sss

--- client-a/src/main/java/com/james/spring/cloud/clienta/controller/TestController.java
+++ client-a/src/main/java/com/james/spring/cloud/clienta/controller/TestController.java
@@ -1,20 +1,14 @@
 package com.james.spring.cloud.clienta.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @RestController
 public class TestController {
 
-    @Value("${config.name}")
-    private String name;
-
-    @GetMapping("/name")
+    @GetMapping("/hello")
     public String getName() {
-        return name;
+        return "hello";
     }
 
 }
